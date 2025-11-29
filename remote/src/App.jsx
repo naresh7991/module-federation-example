6757c3ac02d6e79dcb41c7adfa04c792189140a2
naresh7991 <49777651+naresh7991@users.noreply.github.com>
2025-11-30 01:34:18 +0530

    add host and remote applicaton code

---
+++ remote/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from './Button'
+import useCount from './store'
+
+function App() {
+  const [count, setCount] = useCount()
+
+  return (
+    <>
+        <h1>Remote Application</h1>
+    <Button/>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
